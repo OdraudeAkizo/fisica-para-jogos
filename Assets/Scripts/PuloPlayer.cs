ad66c826b7b9ce50e5f057a4797bf94e48fb2fa8
Odraude <odraudeakizo123567@gmail.com>
2019-10-16 17:44:30 -0300

    pulo

---
+++ Assets/Scripts/PuloPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuloPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool noChao = false;
+    public float chaoCheckRaio = 0.2f;
+    public Transform ChaoCheck;
+    public LayerMask OQueEChao;
+    void Start()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        noChao = Physics2D.OverlapCircle (ChaoCheck.position, chaoCheckRaio, OQueEChao);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (noChao && Input.GetButton("Jump"))
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 30));
+    }
+
+    
+}
