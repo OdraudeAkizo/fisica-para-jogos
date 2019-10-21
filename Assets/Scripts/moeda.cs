43b87e4e2855d4b2cf48136b9794ad55ef3feb96
Odraude <odraudeakizo123567@gmail.com>
2019-10-20 22:32:37 -0300

    contador de moedas

--- Assets/Scripts/moeda.cs
+++ Assets/Scripts/moeda.cs
@@ -1,25 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class moeda : MonoBehaviour
 {
 
     // Start is called before the first frame update
+    public int qtdmoeda = 0;
+    public Text txtmoedas; 
     void Start()
     {
         
     }
 
     // Update is called once per frame
+    
     void Update()
     {
-        
+        txtmoedas.text = qtdmoeda.ToString();
     }
 
-    void OnCollisionEnter2D(Collision2D colisao)
+    void OnTriggerEnter2D(Collider2D moeda)
     {
-
+        if (moeda.gameObject.CompareTag("mueda"))
+        {
+            Destroy(moeda.gameObject);
+            qtdmoeda++;
+        }
     }
 
 }
